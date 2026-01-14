76b6cc10ac78c926fd173ec9883b07d556aa2124
seunghee <seungjj1104@gmail.com>
2026-01-14 17:25:04 +0900

    login, register 추가

--- app.js
+++ app.js
@@ -14,24 +14,52 @@ let data_set = [
 
 app.get("/mem", (req, res) => {
     console.log("서버 실행 mem 통신 성공")
+
+    // // 지연 구현
+    // const end = Date.now() + 3000;
+    // for (; Date.now() < end;) {
+
+    // }
     res.json(data_set);
 })
-app.get("/mem/:id", (req, res)=>{
+
+app.post("/login", (req, res) => {
+    // console.log("req.body: ", req.body)
+    const member = data_set.filter(d => d.id === req.body.id)[0]
+
+    if (member) {
+        if(member.pwd === req.body.pwd)
+            res.json({ success: 0, message: "로그인 성공" })
+        }
+    res.json({ success: 1, message: "로그인 실패" })
+})
+
+app.post("/register", (req, res) => {
+    const member = data_set.filter(d => d.id === req.body.id)[0]
+    if (member) {
+        res.json({ success: 1, message: "존재하는 회원" })
+    }else{
+        data_set = data_set.concat(req.body)
+        res.json({ success: 0, message: "회원가입 성공" })
+    }
+})
+
+app.get("/mem/:id", (req, res) => {
     res.json(data_set.filter((mem) => mem.id === req.params.id)[0]);
 })
 
-app.get("/mem/delete/:id", (req, res)=>{
+app.get("/mem/delete/:id", (req, res) => {
     data_set = data_set.filter((mem) => mem.id !== req.params.id)
     res.json({ success: true, message: "삭제 완료" })
 })
 
-app.post("/mem/add", (req, res)=>{
+app.post("/mem/add", (req, res) => {
     const user = req.body
     data_set = data_set.concat(user)
     res.json({ success: true, message: "추가 완료", data: user })
 })
 
-app.post("/mem/modify", (req, res)=>{
+app.post("/mem/modify", (req, res) => {
     const user = req.body
     data_set = data_set.map(mem => mem.id === user.id ? user : mem)
     res.json({ success: true, message: "수정 완료", data: user })
